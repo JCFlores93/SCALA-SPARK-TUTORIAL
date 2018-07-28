861a5bef7b5624f1740ba1bdab9ebbb41b76f3b6
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2018-07-28 12:25:21 -0500

    sorted word count

--- src/main/scala/com/sparkTutorial/pairRdd/sort/SortedWordCountProblem.scala
+++ src/main/scala/com/sparkTutorial/pairRdd/sort/SortedWordCountProblem.scala
@@ -1,5 +1,7 @@
 package com.sparkTutorial.pairRdd.sort
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 
 object SortedWordCountProblem {
 
@@ -13,5 +15,23 @@ object SortedWordCountProblem {
        bag : 176
        ...
      */
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setAppName("sortedWordCountProblem")
+      .setMaster("local[3]")
+    val sc = new SparkContext(conf)
+    val lines = sc.textFile("in/word_count.text")
+    val wordRDD = lines.flatMap(line => line.split(" "))
+        .map(word => (word,1))
+        .reduceByKey((x, y) => x +y)
+    val countToWordParis = wordRDD.map(wordToCount => (wordToCount._2, wordToCount._1))
+    val sortedCountToWordParis = countToWordParis.sortByKey(ascending = false)
+        .map(countToWord => (countToWord._2, countToWord._1))
+
+    for ((word, count) <- sortedCountToWordParis.collect()) println(word + " : " + count)
+
+  }
+
+
 }
 
